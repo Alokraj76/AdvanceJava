2b3a797717fad527621d2453d1d57de0a63ab61a
Alok Raj <alokra7651@gmail.com>
2026-02-02 13:24:09 +0530

    advance java added

---
+++ Hello.java
@@ -0,0 +1,15 @@
+class Display
+{
+    void displayInfo()
+    {
+        System.out.println("Hello, World!");
+    }
+}
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        Display d = new Display();
+        d.displayInfo();
+    }
+}
