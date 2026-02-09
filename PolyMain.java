ffffd8a6c94c1f0feae54da4d5a3d6ff7f900adf
Alok Raj <alokra7651@gmail.com>
2026-02-09 13:01:19 +0530

    poly

--- PolyMain.java
+++ PolyMain.java
@@ -1,17 +1,40 @@
 class Animal
 {
-    String name;
-    String sound;
+    static String name;
+    static String sound;
     Animal()
     {
-        this("cat","");
+        this("cat",""); //caliing constructor
     }
     Animal(String name,String sound)
     {
         this.name = name;
         this.sound = sound;
     }
-    void getInfo()
+    static void getInfo()
+    {
+        System.out.println("Name= "+name);
+        System.out.println("Sound= "+sound);
+    }
+    void getInfo(String name, String sound,int legs)
+    {
+        System.out.println("Name= "+name);
+        System.out.println("Sound= "+sound);
+        System.out.println("NO. of legs= "+legs);
+    }
+}
+class Cat extends Animal
+{
+    Cat(String name, String sound)
+    {
+        super(name,sound);
+    }
+    static void getInfo()
+    {
+        System.out.println("Cat Name="+name);
+        System.out.println("Cat Sound="+sound);
+    }
+     void getInfo(String name, String sound) //overloading in sub class
     {
         System.out.println("Name= "+name);
         System.out.println("Sound= "+sound);
@@ -21,7 +44,12 @@ public class PolyMain
 {
     public static void main(String[] args) 
     {
-        Animal an = new Animal();
-        an.getInfo();    
+        // Animal an = new Animal();
+        // an.getInfo();  
+        // an.getInfo("Cat","Meow",4);  
+        Cat c1 = new Cat("Cat2","Meow");
+        c1.getInfo();
+        Animal a1 = new Cat("Cat2","Meow");
+        a1.getInfo();
     }
 }
